f332f170a9d8eff0adfa2cc29d957e559c8f47fa
Hovo33651 <hovogevorgyan94@mail.ru>
2022-03-17 17:16:18 +0400

    Health Advisor, still empty project, only has libs

---
+++ src/main/java/com/example/healthadvisors/HealthAdvisorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthadvisors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAdvisorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAdvisorsApplication.class, args);
+    }
+
+}

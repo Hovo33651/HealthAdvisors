f378199deca4ed94c02c800aee1093a664f4a8ba
Hovo33651 <hovogevorgyan94@mail.ru>
2022-03-19 18:28:57 +0400

    security configuration, not complete yet

---
+++ src/main/java/com/example/healthadvisors/config/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.healthadvisors.config;
+
+import com.example.healthadvisors.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/").permitAll();
+
+    }
+}
